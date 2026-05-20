1622609760cde5e1fee1b8fdf996f2b3995026ee
bobbadoo15 <brinton.montgomery@gmail.com>
2026-05-20 08:51:20 -0600

    Moved units into src, created subfolders in src

---
+++ src/main.f90
@@ -0,0 +1,7 @@
+!! This is the main program for the flight simulation. It will initialize the simulation, run the main loop, and handle any necessary cleanup.
+
+program flight_simulation
+    use units_m
+    implicit none
+    
+end program flight_simulation
--- src/units_m.f90
+++ src/units_m.f90
@@ -1,2 +1,6 @@
 ! This file contains the unit definitions/conversions for the flight simulation.
 
+module units_m
+    implicit none
+    
+end module units_m
