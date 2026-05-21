359f1cd27e25bacc9bd129a787a103b41a42905b
bobbadoo15 <brinton.montgomery@gmail.com>
2026-05-20 21:55:59 -0600

    Updated Units file

--- src/units_m.f90
+++ src/units_m.f90
@@ -2,27 +2,91 @@
 
 module units_m
     implicit none
-    !! Constants
-    real, parameter :: pi = 3.1415926535897932384626433832795, &
-                       g_ssl = 9.80665, &        ! Standard gravity at sea level in m/s^2
-                       R_E_Z = 6356766, &        ! Earth radius for Geopotential altitude in m
-                       R_E = 6366707.01949371, & ! Mean radius of the earth at sea level in m
-                       R = 287.0528, &           ! Gas constant for dry air in J/(kg*K)
-                       gamma = 1.4               ! Heat capacity ratio
+    !! =====================================================
+    !!                     CONSTANTS
+    !! =====================================================
+    real, parameter :: pi    = 3.1415926535897932384626433832795, &
+                       e     = 2.718281828459045, &
+                       g_si  = 9.80665, &            ! Standard gravity at sea level in m/s^2
+                       g_us  = 32.1740485564304, &   ! Standard gravity at sea level in ft/s^2
+                       R_E_Z = 6356766, &            ! Earth radius for Geopotential altitude in m
+                       R_E   = 6366707.01949371, &   ! Mean radius of the earth at sea level in m
+                       R     = 287.0528, &           ! Gas constant for dry air in J/(kg*K)
+                       gamma = 1.4                   ! Heat capacity ratio
+
+    !! =====================================================
+    !!                 CONVERSION FACTORS
+    !! =====================================================
     
-    !! Angle conversions
-    real, parameter :: d2r = pi / 180.0, & ! Degrees to radians
-                       r2d = 180.0 / pi    ! Radians to degrees
+    !! Angles
+    real, parameter :: d2r = pi / 180.0, &   ! Degrees to radians
+                       r2d = 180.0 / pi, &   ! Radians to degrees
+                       rev = 0.5 / pi        ! Radians to revolutions
     
-    !! English to SI unit conversions
-    real, parameter :: ft2m = 0.3048, &           ! Feet to meters
-                       lbm2kg = 0.45359237, &     ! Pound-mass to kilograms
-                       lbf2N = 4.4482216152605, & ! Pound-force to newtons
-                       F2K = 5./9., &             ! Fahrenheit to kelvin
-                       slug2kg = 14.5939029, &    ! Slugs to kilograms
-                       psf2pa = 47.880258, &      ! Pressure conversion: lbf/ft^2 to pascal, or N/m^2
-                       ed2sid = 515.379, &        ! Density conversion: slug/ft^3 to kg/m^3
-                       ev2siv = 47.880258         ! Viscosity conversion: slug/(ft*s) to kg/(m*s)
+    !! US to SI
+    real, parameter :: psf2pa  = 47.880258, &        ! Pressure conversion: lbf/ft^2 to pascal, or N/m^2
+                       ed2sid  = 515.379, &          ! Density conversion: slug/ft^3 to kg/m^3
+                       ev2siv  = 47.880258           ! Viscosity conversion: slug/(ft*s) to kg/(m*s)
+
+    !! Desired time from seconds
+    real, parameter :: minute = 1. / 60., &
+                       hour   = 1. / 3600., &
+                       day    = 1. / 86400., &
+                       week   = 1. / 604800., &
+                       month  = 1. / 2628000., &
+                       year   = 1. / 31536000.
+
+    !! Desired length from Feet
+    real, parameter :: m     = 0.3048, &
+                       inch  = 12., &
+                       yard  = 1. / 3., &
+                       mile  = 1. / 5280., &
+                       nmile = m / 1852.
+
+    !! Desired velocity from ft/s
+    real, parameter :: metps = 0.3048, &
+                       kph   = 1.09728, &
+                       mph   = 0.68181818, &
+                       knots = 0.5924838
+
+    !! Desired acceleration from ft/s^2
+    real, parameter :: 
+
+    !! Desired mass from US to SI
+    real, parameter :: lbm2kg  = 0.45359237, & ! Pound-mass to kilograms
+                       slug2kg = 14.5939029    ! Slugs to kilograms
+
+    !! Desired temperature from Fahrenheit
+    real, parameter :: kelvin = 255.927778., &
+                       celcius = -17.2222222., &
+                       rankine = 460.67.
+
+    !! Desired force from lbf = slug * ft/s^2
+    real, parameter :: N   = 4.4482216152605
+
+
+    !! =====================================================
+    !!                    SI Information
+    !! =====================================================
+
+    !! Prefixes
+    real, parameter ::  atto  = 1.e-18, &   ! a
+                        femto = 1.e-15, &   ! f
+                        pico  = 1.e-12, &   ! p
+                        nano  = 1.e-9, &    ! n
+                        micro = 1.e-6, &    ! u
+                        milli = 1.e-3, &    ! m
+                        centi = 1.e-2, &    ! c
+                        deci  = 1.e-1, &    ! d
+                        deka  = 1.e1, &     ! da
+                        hecto = 1.e2, &     ! h
+                        kilo  = 1.e3, &     ! k
+                        mega  = 1.e6, &     ! M
+                        giga  = 1.e9, &     ! G
+                        tera  = 1.e12, &    ! T
+                        peta  = 1.e15, &    ! P
+                        exa   = 1.e18       ! E
+                       
 
 contains
     
