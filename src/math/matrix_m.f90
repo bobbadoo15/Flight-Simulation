d85e4ecd8fdba4213ff4826c06f67add36ef39a2
bobbadoo15 <brinton.montgomery@gmail.com>
2026-05-22 22:32:23 -0600

    created contains and a function in matrix_m

--- src/math/matrix_m.f90
+++ src/math/matrix_m.f90
@@ -2,11 +2,22 @@
 module matrix_m
     use vector_m
     implicit none
+    
     type :: matrix
     contains
     end type matrix
 
     ! ID matrix - reshape converts 1d array into 3d array, filling the columns first
     real, dimension(3,3) :: id_mat = reshape([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0], shape=[3,3])
-    
+
+contains
+
+    !! ============================================
+    !!              Matrix Operations
+    !! ============================================
+
+    ! Matrix multiplication
+    function m_times_m()
+    end function m_times_m
+
 end module matrix_m
