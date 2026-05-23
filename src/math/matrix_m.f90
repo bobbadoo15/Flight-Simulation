f8b35d72542c58838d3288b6d3437d75d370a829
bobbadoo15 <brinton.montgomery@gmail.com>
2026-05-22 22:45:26 -0600

    included interface procedures and type procedures

--- src/math/matrix_m.f90
+++ src/math/matrix_m.f90
@@ -2,10 +2,29 @@
 module matrix_m
     use vector_m
     implicit none
-    
+
     type :: matrix
     contains
+        procedure :: mult => m_times_m
+        procedure :: t    => m_transpose
     end type matrix
+
+    interface operator (*)
+        procedure m_times_m
+    end interface operator (*)
+
+    interface operator (/)
+    end interface operator (/)
+
+    interface operator (+)
+    end interface operator (+)
+
+    interface operator (-)
+    end interface operator (-)
+
+    interface operator (.t.)
+        procedure m_transpose
+    end interface operator (.t.)
 
     ! ID matrix - reshape converts 1d array into 3d array, filling the columns first
     real, dimension(3,3) :: id_mat = reshape([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0], shape=[3,3])
@@ -19,5 +38,9 @@ contains
     ! Matrix multiplication
     function m_times_m()
     end function m_times_m
+
+    ! Matrix transposition
+    function m_transpose()
+    end function m_transpose
 
 end module matrix_m
