8fb81b4170d5fb182b8322453956fb5115e0383c
bobbadoo15 <brinton.montgomery@gmail.com>
2026-05-22 20:42:11 -0600

    Included some if conditionals in euler and started setting the structure for vectors

--- src/math/euler_angles_m.f90
+++ src/math/euler_angles_m.f90
@@ -5,39 +5,79 @@ module euler_angles_m
     use vector_m
     implicit none
 
+    type rot_mat
+        real, dimension(3,3) :: mat
+    contains        
+    end type rot_mat
+
 contains
 
+    !! =========================================
+    !!                SUBROUTINES
+    !! =========================================
+
+    subroutine rotation_matrix(m)
+        class(rot_mat), intent(inout) :: m
+        
+    end subroutine rotation_matrix
+
+    subroutine rotation_matrix_transpose(m)
+        class(rot_mat), intent(inout) :: m
+    end subroutine rotation_matrix_transpose
+
+    !! =========================================
+    !!                 FUNCTIONS
+    !! =========================================
+
     ! Function that first rotates about the z-axis of the base coordinate system by the azimuth angle, psi = phi_z; 0 <= psi <= 360, order 1
-    function z_rotation_matrix(psid) result(A)
+    function z_rotation_matrix(psid) result(z_mat)
         real, intent(in)     :: psid       ! psi in degrees
-        real, dimension(3,3) :: A
+        real, dimension(3,3) :: z_mat
         real                 :: s, c, psir ! psi in radians
-        psir = psid * d2r
-        s = sin(psir)
-        c = cos(psir)
-        A = reshape([c, -s, 0.0, s, c, 0.0, 0.0, 0.0, 1.0], shape=[3,3])
+        ! Error handling for angle range
+        if ((psid >= 0.0) .and. (psid <= 360.0)) then
+            psir = psid * d2r
+            s = sin(psir)
+            c = cos(psir)
+            z_mat = reshape([c, -s, 0.0, s, c, 0.0, 0.0, 0.0, 1.0], shape=[3,3])
+        else
+            z_mat = 0.0
+            write (*,*) "The azimuth angle is not in the range between 0 and 360."            
+        end if
     end function z_rotation_matrix
 
     ! Function that rotates after the z-axis, about the elevation (y-axis), of the base coordinate system by the elevation angle, theta = phi_y; -90 <= psi <= 90, order 2
-    function y_rotation_matrix(thetad) result(A)
+    function y_rotation_matrix(thetad) result(y_mat)
         real, intent(in)     :: thetad       ! theta in degrees
-        real, dimension(3,3) :: A
+        real, dimension(3,3) :: y_mat
         real                 :: s, c, thetar ! theta in radians
-        thetar = thetad * d2r
-        s = sin(thetar)
-        c = cos(thetar)
-        A = reshape([c, 0.0, s, 0.0, 1.0, 0.0, -s, 0.0, c], shape=[3,3])
+        ! Error handling for angle range
+        if ((thetad >= -90.0) .and. (thetad <= 90.0)) then
+            thetar = thetad * d2r
+            s = sin(thetar)
+            c = cos(thetar)
+            y_mat = reshape([c, 0.0, s, 0.0, 1.0, 0.0, -s, 0.0, c], shape=[3,3])
+        else
+            y_mat = 0.0
+            write (*,*) "The elevation angle is not in the range between -90 and 90."            
+        end if
     end function y_rotation_matrix
 
     ! Function that rotates last about the x-axis of the base coordinate system by the bank angle, phi = phi_z; -180 <= phi <= 180, order 3
-    function x_rotation_matrix(phid) result(A)
+    function x_rotation_matrix(phid) result(x_mat)
         real, intent(in)     :: phid       ! phi in degrees
-        real, dimension(3,3) :: A
+        real, dimension(3,3) :: x_mat
         real                 :: s, c, phir ! phi in radians
-        phir = phid * d2r
-        s = sin(phir)
-        c = cos(phir)
-        A = reshape([1.0, 0.0, 0.0, 0.0, c, -s, 0.0, s, c], shape=[3,3])
+        ! Error handling for angle range
+        if ((phid >= -180.0) .and. (phid <= 180.0)) then
+            phir = phid * d2r
+            s = sin(phir)
+            c = cos(phir)
+            x_mat = reshape([1.0, 0.0, 0.0, 0.0, c, -s, 0.0, s, c], shape=[3,3])
+        else
+            x_mat = 0.0
+            write (*,*) "The bank angle is not in the range between -180 and 180."            
+        end if
     end function x_rotation_matrix
     
 end module euler_angles_m
--- src/math/vector_m.f90
+++ src/math/vector_m.f90
@@ -6,8 +6,103 @@ module vector_m
 
     ! ID matrix - reshape converts 1d array into 3d array, filling the columns first
     real, dimension(3,3) :: id_mat = reshape([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0], shape=[3,3])
+    
+    type :: vector
+        real :: x, y, z
+    contains
+        ! This procedure calls the function whenever its 'nickname is used'
+    end type vector
 
+    ! Whenever + is used with the vector type, this operator calls the following functions
+    interface operator (+)
+        procedure v_add_v
+    end interface operator (+)
+
+    ! Whenever - is used with the vector type, this operator calls the following functions
+    interface operator (-)
+        procedure v_sub_v
+    end interface operator (-)
+
+    ! Whenever * is used with the vector type, this operator calls the following functions
+    interface operator (*)
+        procedure v_times_s
+        procedure s_times_v
+    end interface operator (*)
+
+    ! Whenever / is used with the vector type, this operator calls the following functions
+    interface operator (/)
+        procedure v_div_s
+    end interface operator (/)
+
+    ! Whenever .cross. is used with the vector type, this operator calls the following functions
+    interface operator (.cross.)
+        procedure v_cross_v
+    end interface operator (.cross.)
+
+    ! Whenever .dot. is used with the vector type, this operator calls the following functions
+    interface operator (.dot.)
+        procedure v_dot_v
+    end interface operator (.dot.)
+
+    ! Whenever .create. is used with the vector type, this operator calls the following functions
+    interface operator (.create.)
+        procedure v_new
+    end interface operator (.create.)
 
 contains
+
+    !! ============================================
+    !!              Vector Operations
+    !! ============================================
+
+    ! Adding vectors
+    function v_add_v(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_add_v
+
+    ! Cross multiplying vectors
+    function v_cross_v(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_cross_v
+
+    ! Dot multiplying vectors
+    function v_dot_v(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_dot_v
+
+    ! Subtracting vectors
+    function v_sub_v(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_sub_v
+
+    !! ============================================
+    !!        Vector Operations with Scalars
+    !! ============================================
+
+    ! Dividing a vector with a scalar
+    function v_div_s(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_div_s
+
+    ! Multiplying a vector with a scalar
+    function v_times_s(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function v_times_s
+
+    ! Multiplying a scalar with a vector
+    function s_times_v(v1, v2)
+        class(vector), intent(in) :: v1, v2
+    end function s_times_v
+
+    !! ============================================
+    !!        Vector Operations with Points
+    !! ============================================
+
+    !! ============================================
+    !!            Misc. Vector Relations
+    !! ============================================
+
+    function v_new()
+    end function v_new
 
 end module vector_m
