d83399a7a0267be02c1f99fe66d3ef9203a836b3
bobbadoo15 <brinton.montgomery@gmail.com>
2026-06-01 22:22:20 -0600

    Organized .bat file

--- build.bat
+++ build.bat
@@ -1,11 +1,20 @@
 @echo off
 setlocal
+:: DISPLAYING PROGRAM IMAGE AT BEGINNING OF COMPILING
 type image.txt
 
-REM ======= COMPILER =======
+:: ============================================================
+::          Flight-Simulation Build, Compile, and Run
+::                     GFortran 90 | OOP
+:: ============================================================
+
+:: ======= COMPILER =======
 set GF=gfortran
 
-REM ======= DIRECTORIES =======
+:: ======= FLAGS =======
+
+
+:: ======= DIRECTORIES =======
 set BIN_DIR=bin
 set MOD_DIR=mod
 set OBJ_DIR=obj
@@ -14,25 +23,52 @@ set OUT_DIR=output
 set SRC_DIR=src
 set EXE_DIR=%BIN_DIR%\falcon.exe
 
-REM ---- Create bin, obj, and mod if they don't exist ----
+:: ---- Create bin, obj, and mod if they don't exist ----
 if not exist build\%BIN_DIR% mkdir build\%BIN_DIR%
 if not exist build\%MOD_DIR% mkdir build\%MOD_DIR%
 if not exist build\%OBJ_DIR% mkdir build\%OBJ_DIR%
 
-REM ======= COMPILE FORTRAN FILES =======
-REM ---- List files in order ----
+:: ======= COMPILE FORTRAN FILES =======
+:: ---- Display messages for compilation ----
+echo.
+echo ============================================================
+echo  Compiling modules...
+echo ============================================================
+echo.
+
+:: ---- List files in order ----
 set FILES= ^
     %SRC_DIR%\units_m.f90 ^
     %SRC_DIR%\math\vector_m.f90 ^
     %SRC_DIR%\math\matrix_m.f90 ^
     %SRC_DIR%\math\euler_angles_m.f90
+    %SRC_DIR%\falcon.f90
 
-REM ---- Loop through each file to compile ----
+:: ---- Loop through each file to compile ----
 for %%f in %FILES% do (
     
 )
 
-REM ======= LINKER =======
+:: ======= LINKER =======
+echo.
+echo ============================================================
+echo  Linking...
+echo ============================================================
+echo.
 
-ECHO Build complete!
+:: ======= RUN =======
+echo.
+echo ============================================================
+echo  Running FlightSim...
+echo ============================================================
+echo.
+%EXE%
+
+:: ======= COMPLETE PROGRAM =======
+echo.
+echo ============================================================
+echo  Build Complete!
+echo ============================================================
+echo.
+
 endlocal
