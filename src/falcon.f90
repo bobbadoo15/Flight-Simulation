079ee10d3dd840da2a408c870e12fbed83f79930
bobbadoo15 <brinton.montgomery@gmail.com>
2026-06-01 21:58:52 -0600

    Organized my structure

--- src/falcon.f90
+++ src/falcon.f90
@@ -2,6 +2,12 @@
 
 program flight_simulation
     use units_m
+    use vector_m
+    use matrix_m
+    use euler_angles_m
     implicit none
+
+    ! Call for euler angles
+    
     
 end program flight_simulation
